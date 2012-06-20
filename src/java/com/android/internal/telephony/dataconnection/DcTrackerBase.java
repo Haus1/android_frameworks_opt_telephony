4951b2c270c52e7bcc3c9d8c5509846defff66ed
Alex Yakavenka <ayakav@codeaurora.org>
2012-06-20 16:20:31 -0700

    Telephony: Ignore data registration when bringing down call
    Bring down data call even if we have it while data registration indicates
    phone is in unregistered state.
    getAnyDataEnabled will return false if phone is not registered hence
    user disabling data while phone is unregistered will have no effect
    (and data call will remain there indefinitely).
    
    Change-Id: I72cf1fb8b89cad5bb8595233721d1ac315750dbf
    CRs-Fixed: 372219
    (cherry picked from commit 0bf3869ffd41af11d2f144ac0fec174a582fd023)
    (cherry picked from commit 422e033ca7fb169362ddae65448f96ae681787d5)
    (cherry picked from commit 8b009c7dadc1cfc213d86f7d6641da36c06ec23a)

--- src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
@@ -1306,7 +1306,6 @@ public abstract class DcTrackerBase extends Handler {
 
     protected void onSetUserDataEnabled(boolean enabled) {
         synchronized (mDataEnabledLock) {
-            final boolean prevEnabled = getAnyDataEnabled();
             if (mUserDataEnabled != enabled) {
                 mUserDataEnabled = enabled;
                 Settings.Global.putInt(mPhone.getContext().getContentResolver(),
@@ -1319,12 +1318,11 @@ public abstract class DcTrackerBase extends Handler {
                         notifyOffApnsOfAvailability(Phone.REASON_DATA_DISABLED);
                     }
                 }
-                if (prevEnabled != getAnyDataEnabled()) {
-                    if (!prevEnabled) {
-                        onTrySetupData(Phone.REASON_DATA_ENABLED);
-                    } else {
-                        onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
-                    }
+
+                if (enabled) {
+                    onTrySetupData(Phone.REASON_DATA_ENABLED);
+                } else {
+                    onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
                 }
             }
         }
@@ -1335,15 +1333,12 @@ public abstract class DcTrackerBase extends Handler {
 
     protected void onSetPolicyDataEnabled(boolean enabled) {
         synchronized (mDataEnabledLock) {
-            final boolean prevEnabled = getAnyDataEnabled();
             if (sPolicyDataEnabled != enabled) {
                 sPolicyDataEnabled = enabled;
-                if (prevEnabled != getAnyDataEnabled()) {
-                    if (!prevEnabled) {
-                        onTrySetupData(Phone.REASON_DATA_ENABLED);
-                    } else {
-                        onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
-                    }
+                if (enabled) {
+                    onTrySetupData(Phone.REASON_DATA_ENABLED);
+                } else {
+                    onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
                 }
             }
         }
