aea585c71bc53cf235588ba892722965dc0800dc
Jon Haus <haus.xda@gmail.com>
2013-12-14 17:03:08 -0500

    Revert "Fix Signal Bars only showing little signal"
    This compromises some core functionality of the RIL and creates obscure errors
    that are very time consuming to troubleshoot. The signal bars are purely
    cosmetic and would be better adjusted from the SystemUI.
    
    This reverts commit 3ecbebd31abc6be079474ef7e6780f04ad192fa8.

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -923,7 +923,7 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
 
         if (hasRilDataRadioTechnologyChanged) {
             mPhone.setSystemProperty(TelephonyProperties.PROPERTY_DATA_NETWORK_TYPE,
-                    ServiceState.rilRadioTechnologyToString(mSS.getRilDataRadioTechnology()));
+                    ServiceState.rilRadioTechnologyToString(mSS.getRilVoiceRadioTechnology()));
         }
 
         if (hasRegistered) {
